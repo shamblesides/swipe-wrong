03d051e33d950db6aef39458fea57a1ab4f7b3a4
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-01-27 00:59:29 -0700

    Cool web app for picking keywords from the big list

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('First commit');
+const express = require('express');
+
+const db = {};
+
+const app = express();
+app.use(express.json());
+app.post('/api/word', (req, res) => {
+    const { word, used } = req.body;
+    console.log(`[${used?'YES!':'undo'}]    "${word}"`);
+    db[word] = used;
+    res.sendStatus(204);
+})
+app.use(express.static('web'))
+
+app.listen(80, () => console.log('---WORD SERVER UP---'));
