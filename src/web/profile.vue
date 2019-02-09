e5d625fe0aa887dd5bf5d47a34fcc2f8f3419bd7
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-09 02:01:32 -0700

    functional barebones profile page

--- src/web/profile.vue
+++ src/web/profile.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>
-    {{$route.params.id}}
+  <div id="profile">
+    <div class="word" :class="{selected:wordBlock.selected}" :id="'word'+i" v-for="(wordBlock, i) of words" :key="i" :style="{ left: wordBlock.x+'px', top: wordBlock.y+'px' }">
+      {{ wordBlock.word }}
+    </div>
   </div>
 </template>
 
 <script>
-
+  module.exports = {
+    data: function() {
+      return {
+        words: []
+      }
+    },
+    async mounted() {
+      this.words = (await axios.get('/api/profile/'+this.$route.params.id)).data
+    },
+  };
 </script>
+
+<style scoped>
+  .word {
+    padding: 10px;
+    position: absolute;
+    animation: wobble 2s infinite;
+    animation-direction: alternate;
+    font-size: 22px;
+    white-space: nowrap;
+  }
+  @keyframes wobble {
+    0% {
+      transform: rotate(5deg);
+    }
+    100% {
+      transform: rotate(-5deg);
+    }
+  }
+</style>
