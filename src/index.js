fbb243be68338bcd4cad5956f164d7c000606ae5
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-10 18:06:22 -0700

    move function randomWord into its own file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const nanoid = require('nanoid/generate');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+const randomWord = require('./words');
 
 if (!fs.existsSync('./secret')) {
     const secret = crypto.randomBytes(256/8)
@@ -12,43 +13,6 @@ if (!fs.existsSync('./secret')) {
 }
 const jwtSecret = fs.readFileSync('./secret');
 const auth = expressJwt({ secret: jwtSecret })
-
-const freqMap = fs.readFileSync(path.resolve(__dirname, './data/phrases.txt'))
-    .toString()
-    .split('\n')
-    .map(line => line.trim())
-    .map(line => ({
-        weight: +line.split(' ')[0],
-        word: line.substr(line.indexOf(' ')+1),
-    }))
-    .map(({ word, weight }) => ({
-        word,
-        weight: weight ** 0.6
-    }))
-    .reduce((map, { word, weight }) => word ? map.set(word, weight) : map, new Map())
-
-const [db, randMax] = fs.readFileSync(path.resolve(__dirname, './data/chosen.txt'))
-    .toString()
-    .split('\n')
-    .map(word => ({ word, weight: freqMap.get(word) }))
-    .sort((a, b) => (b.weight - a.weight) || (a.word.localeCompare(b.word)))
-    .reduce(([arr, total], { word, weight }) => {
-        arr.push({ word, min: total });
-        return [arr, total+weight];
-    }, [[], 0])
-
-function randomWord() {
-    const i = Math.floor(Math.random() * randMax);
-    
-    let l = 0, r = db.length - 1;
-    while (r > l) {
-        const mid = Math.ceil(l + (r-l)/2);
-        const { min } = db[mid];
-        if (i < min) r = mid - 1;
-        else l = mid;
-    }
-    return db[l].word;
-}
 
 const profiles = {};
 
