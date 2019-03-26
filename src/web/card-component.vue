e05d04936de278c98d1aa5e8142b5a3e0f1094a4
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-03-25 22:19:31 -0600

    componentize profile card

---
+++ src/web/card-component.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card">
+    <img :src="pic" id="profile-image">
+    <h2>{{ name }}</h2>
+    <div id="words-area">
+      <div class="word" v-for="(wordBlock, i) of words" :key="'word'+i" :style="{ left: wordBlock.x+'px', top: wordBlock.y+'px' }">
+        {{ wordBlock.word }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    props: {
+      pic: String,
+      name: String,
+      words: Array,
+    }
+  }
+</script>
+
+<style scoped>
+  .card {
+    background-color: #F9D0DA;
+    width: 420px;
+    margin: auto;
+    box-shadow: -10px 10px 0 rgba(0,0,0,0.5);
+  }
+  #profile-image {
+    margin: 20px auto 0;
+    width: 90%;
+  }
+  .card h2 {
+    margin: 5px auto 0;
+  }
+  #words-area {
+    position: relative;
+    height: 290px;
+    margin: 0 20px 40px;
+  }
+  .word {
+    padding: 10px;
+    position: absolute;
+    animation: wobble 2s infinite;
+    animation-direction: alternate;
+    font-size: 22px;
+    white-space: nowrap;
+  }
+  @keyframes wobble {
+    0% {
+      transform: rotate(5deg);
+    }
+    100% {
+      transform: rotate(-5deg);
+    }
+  }
+</style>
--- src/web/profile.vue
+++ src/web/profile.vue
@@ -3,17 +3,9 @@
     <template v-if="!words">
       Loading profile...
     </template>
-    <template>
-    <h2>Looking Good!</h2>
-    <div class="card">
-      <img :src="pic" id="profile-image">
-      <h2>{{ name }}</h2>
-      <div id="words-area">
-        <div class="word" :class="{selected:wordBlock.selected}" :id="'word'+i" v-for="(wordBlock, i) of words" :key="i" :style="{ left: wordBlock.x+'px', top: wordBlock.y+'px' }">
-          {{ wordBlock.word }}
-        </div>
-      </div>
-    </div>
+    <template v-else>
+      <h2>Looking good!</h2>
+      <card :name="name" :pic="pic" :words="words"/>
     </template>
   </div>
 </template>
@@ -36,45 +28,14 @@
       this.name = name;
       this.pic = pic;
     },
+    components: {
+      'card': httpVueLoader('card-component.vue'),
+    },
   };
 </script>
 
 <style scoped>
   #profile {
     text-align: center;
-  }
-  #words-area {
-    position: relative;
-    height: 290px;
-    margin: 0 20px 40px;
-  }
-  .card {
-    background-color: #F9D0DA;
-    width: 420px;
-    margin: auto;
-    box-shadow: -10px 10px 0 rgba(0,0,0,0.5);
-  }
-  .card h2 {
-    margin: 5px auto 0;
-  }
-  .word {
-    padding: 10px;
-    position: absolute;
-    animation: wobble 2s infinite;
-    animation-direction: alternate;
-    font-size: 22px;
-    white-space: nowrap;
-  }
-  @keyframes wobble {
-    0% {
-      transform: rotate(5deg);
-    }
-    100% {
-      transform: rotate(-5deg);
-    }
-  }
-  #profile-image {
-    margin: 20px auto 0;
-    width: 90%;
   }
 </style>
