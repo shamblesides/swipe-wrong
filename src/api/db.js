8e3a65dd327d74dbc176f5950a15993d747f5f1e
Nigel Nelson <nigel@prodatakey.com>
2019-08-26 22:42:44 -0600

    no docker

--- src/api/db.js
+++ src/api/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = process.env.APP_MONGO_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/swipe-wrong';
 
 let _db = null;
 
